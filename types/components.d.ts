2ba7ea8e00873054546188a6308b03281d912306
kl <kanglu-email@qq.com>
2024-12-25 17:28:14 +0800

    types/components.d.ts 更新

--- types/components.d.ts
+++ types/components.d.ts
@@ -11,7 +11,6 @@ declare module 'vue' {
     ElBreadcrumb: typeof import('element-plus/es')['ElBreadcrumb']
     ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
     ElButton: typeof import('element-plus/es')['ElButton']
-    ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
     ElContainer: typeof import('element-plus/es')['ElContainer']
     ElDialog: typeof import('element-plus/es')['ElDialog']
     ElDropdown: typeof import('element-plus/es')['ElDropdown']
