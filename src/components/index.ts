37a86336d3eb41bb2a7eca786783b2da55587d6e
kl <kanglu-email@qq.com>
2025-03-21 15:09:03 +0800

    fix: 修复 el-dropdown aria-hidden 报错问题

--- src/components/index.ts
+++ src/components/index.ts
@@ -39,11 +39,11 @@ export {
 
 
 const components = import.meta.glob('./zs-*/*.vue');
-const registerGlobalComponent = (app: { component: (arg0: string, arg1: globalThis.Component) => void; }) => {
+const registerGlobalComponent = (app: { component: (arg0: string, arg1: globalThis.Component) => void; }, prefix: string = 'zs') => {
     for (const [key, value] of Object.entries(components)) {
-
         const name = key.slice(key.lastIndexOf('/') + 1, key.lastIndexOf('.'));
-        app.component(name, defineAsyncComponent(value as any));
+
+        app.component(name.replace('zs', prefix), defineAsyncComponent(value as any));
     }
 };
 
--- src/components/zs-header-user/zs-header-user.vue
+++ src/components/zs-header-user/zs-header-user.vue
@@ -6,12 +6,7 @@
             <ep-caret-bottom />
         </div>
         <template #dropdown>
-            <el-dropdown-menu>
-                <el-dropdown-item v-for="item, index in dropdownList" :key="item.text" :icon="item.icon"
-                    @click="handleDropdown(index, item)">
-                    {{ item.text }}
-                </el-dropdown-item>
-            </el-dropdown-menu>
+            <slot name="dropdownMenu" />
         </template>
     </el-dropdown>
 </template>
@@ -19,31 +14,15 @@
 <script lang="ts" setup>
 import defaultAvatar from '@/assets/images/icon/default_avatar.png';
 
-interface IDropdownList {
-    text: string
-    icon: string
-    click?: () => void
-}
-
 interface IUserInfo {
     name?: string
     avatar?: string
-    dropdownList?: IDropdownList[]
 }
 
 withDefaults(defineProps<IUserInfo>(), {
     name: 'nickname',
     avatar: defaultAvatar,
 })
-const emits = defineEmits(['dropdown']);
-
-function handleDropdown(currentIndex: number, item: IDropdownList) {
-    item.click?.();
-    emits('dropdown', {
-        currentIndex,
-        item
-    });
-}
 </script>
 
 <style lang="scss" scoped>
