cb522207afaf29bf2ae67054b25eea0a1bcec58e
kl <kanglu-email@qq.com>
2025-05-07 18:17:12 +0800

    feat: 组件zs-title 更新

--- src/components/zs-title/zs-title.vue
+++ src/components/zs-title/zs-title.vue
@@ -1,33 +1,39 @@
 <template>
-  <div class="title-wrap">
-    <p class="title">
-      {{ text }}
-    </p>
-    <slot name="titleRight" />
-  </div>
-  <slot />
+    <div :class="{ 'title-wrap': true, 'border': border }">
+        <p :class="['title', className]">
+            {{ text }}
+        </p>
+        <slot name="titleRight" />
+    </div>
+    <slot />
 </template>
 
 <script lang="ts" setup>
 type TAlign = 'left' | 'right' | 'center'
 
 interface IProps {
-  text: string
-  align?: TAlign
-  background?: string
+    text: string
+    align?: TAlign
+    border?: boolean
+    background?: string
+    className?: string
 }
 
 withDefaults(defineProps<IProps>(), {
-  align: 'left'
+    align: 'left'
 })
 </script>
 
 <style lang="scss" scoped>
 .title-wrap {
-  @apply flex items-center justify-between;
+    @apply flex items-center justify-between;
 }
 
 .title {
-  @apply my-3 text-sm text-#333 fw-600;
+    @apply my-3 text-sm text-#333 fw-600;
+}
+
+.border {
+    @apply b-b-1 b-b-solid b-[var(--el-border-color)];
 }
 </style>
